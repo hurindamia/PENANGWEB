e1c3ea7bf66a6d353a22ccf1cfbfaa5a3b080f6f
ftnizzati <fatin.izzatirozali@gmail.com>
2025-01-11 09:27:41 +0800

    interface change for spot

--- src/components/PictureGallery/PictureGallery.jsx
+++ src/components/PictureGallery/PictureGallery.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import "./PictureGallery.css";
 
 const images = [
-  "/images/penang-bridge.jpg",
-  "/images/penang4.jpg",
-  "/images/penang8.jpg",
-  "/images/penang1.jpg",
-  "/images/penang2.jpg",
-  "/images/penang3.jpg",
-  "/images/penang5.jpg",
-  "/images/penang6.jpg",
-  "/images/penang7.jpg",
+  { src: "/images/penang-bridge.jpg", description: "Penang Bridge" },
+  { src: "/images/penang4.jpg", description: "Penang Clock Tower" },
+  { src: "/images/penang8.jpg", description: "Old Colonial Building" },
+  { src: "/images/penang1.jpg", description: "Trishaw Ride" },
+  { src: "/images/Ceramic.png", description: "Sila Studio Ceramic" },
+  { src: "/images/penang3.jpg", description: "Street Art" },
+  { src: "/images/penang5.jpg", description: "Peranakan Mansion" },
+  { src: "/images/penang6.jpg", description: "Clan Jetty" },
+  { src: "/images/Penanglittleindiaarch.jpg", description: "Little India" },
+  { src: "/images/Kampung Agong.jpg", description: "Kampung Agong" },
+  { src: "/images/komtar.jpg", description: "Komtar" },
+  { src: "/images/entopia.jpg", description: "Entopia" },
 ];
 
 const PictureGallery = () => {
@@ -19,8 +22,15 @@ const PictureGallery = () => {
       <h2>Discover Penang</h2>
       <div className="gallery">
         {images.map((image, index) => (
-          <div className="gallery-item" key={index}>
-            <img src={image} alt={`Penang ${index + 1}`} />
+          <div className="gallery-item flip-card" key={index}>
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={image.src} alt={`Penang ${index + 1}`} />
+              </div>
+              <div className="flip-card-back">
+                <p>{image.description}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
